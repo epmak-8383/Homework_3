ec49c2ffa0c4da8b031f31617f6955e0585e039e
Ermachenkov Maxim <epmak.8383@mail.ru>
2022-12-10 00:43:01 +0300

    третье задание

---
+++ Task_3/Program.cs
@@ -0,0 +1,19 @@
+﻿int EnterData(string text)
+{
+    Console.Write(text);
+    int N = int.Parse(Console.ReadLine());
+    return N;
+}
+
+void Sqr(int N)
+{
+    N=N*N*N;
+    Console.Write(N + ", ");
+}
+int N = EnterData("Введите число: ");
+int i=1;
+while (i<=N)
+{
+    Sqr(i);
+    i++;
+}
